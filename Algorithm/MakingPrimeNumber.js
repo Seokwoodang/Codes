8505c5b4ea733a0bb3cfd953866bf3a2b6661a55
Seokwoo Kang <swoo1429@naver.com>
2022-05-31 22:27:39 +0900

    알고리즘 소수 구하기

--- Algorithm/MakingPrimeNumber.js
+++ Algorithm/MakingPrimeNumber.js
@@ -3,10 +3,9 @@ function solution(nums) {
     let count = 0;
  
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
             for (let k = j + 1; k < nums.length; k++) {
-                const sum=nums[i] + nums[j] + nums[k];
-                if (isprime(sum)){ count += 1;}
+                if (isprime(nums[i] + nums[j] + nums[k])){ count += 1;}
                 }
             }
 
